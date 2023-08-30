6def07f0a6f5524ff3f403e9a08f76a6a7303231
Neuroleptique <corn.xiao@gmail.com>
2023-08-29 23:29:20 -0400

    plotting the dataset

---
+++ main.py
@@ -0,0 +1,49 @@
+from sklearn import datasets
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+iris = datasets.load_iris() #Loading the dataset
+print(iris.keys())
+
+
+iris = pd.DataFrame(
+    data= np.c_[iris['data'], iris['target']],
+    columns= iris['feature_names'] + ['target']
+    )
+
+species = []
+
+for i in range(len(iris['target'])):
+    if iris['target'][i] == 0:
+        species.append("setosa")
+    elif iris['target'][i] == 1:
+        species.append('versicolor')
+    else:
+        species.append('virginica')
+
+
+iris['species'] = species
+
+print(iris.describe())
+
+setosa = iris[iris.species == "setosa"]
+versicolor = iris[iris.species=='versicolor']
+virginica = iris[iris.species=='virginica']
+
+fig, ax = plt.subplots()
+fig.set_size_inches(13, 7) # adjusting the length and width of plot
+
+# lables and scatter points
+ax.scatter(setosa['petal length (cm)'], setosa['petal width (cm)'], label="Setosa", facecolor="blue")
+ax.scatter(versicolor['petal length (cm)'], versicolor['petal width (cm)'], label="Versicolor", facecolor="green")
+ax.scatter(virginica['petal length (cm)'], virginica['petal width (cm)'], label="Virginica", facecolor="red")
+
+
+ax.set_xlabel("petal length (cm)")
+ax.set_ylabel("petal width (cm)")
+ax.grid()
+ax.set_title("Iris petals")
+ax.legend()
+
+plt.show()
